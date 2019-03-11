e03f7b09ad8ca497a395a931566df5e9f3c04bd7
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-11 11:51:43 +0000

    created and routed a separate BMI calculator that computes a person's bmi from their metrics

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.post("/", function(req, res) {
     res.send(`The result is ${result}`);
 });
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmicalculator", function(req, res) {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var n = weight / (height * height);
+
+    res.send(`Your BMI is ${n}`);
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000")
 });
